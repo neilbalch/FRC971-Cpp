99386824818bbffebaed24fad83029471afe6349
Neil B <neilbalch@gmail.com>
2018-04-01 23:08:51 -0700

    Minor formatting

--- MultithreadingExample.cc
+++ MultithreadingExample.cc
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <thread>
 #include <mutex>
+#include <thread>
 
 // Shared resource locker.
 std::mutex m;
@@ -8,23 +8,23 @@ std::mutex m;
 void DoWork() {
   // Lock the mutex or block until locking is possible.
   m.lock();
-  
-  std::cout << "Worker thread: std::this_thread::get_id()=" << std::this_thread::get_id() << std::endl;
+
+  std::cout << "Worker thread: std::this_thread::get_id()="
+            << std::this_thread::get_id() << std::endl;
   std::cin.get();
-  
+
   // Unlock the mutex for other threads to lock.
   m.unlock();
 }
 
-int main()
-{
-  std::cout << "Parent thread: std::this_thread::get_id()=" << std::this_thread::get_id() << std::endl;
+int main() {
+  std::cout << "Parent thread: std::this_thread::get_id()="
+            << std::this_thread::get_id() << std::endl;
 
-    
   std::thread worker1(DoWork);
   std::thread worker2(DoWork);
   std::thread worker3(DoWork);
-  
+
   worker1.join();
   worker2.join();
   worker3.join();
